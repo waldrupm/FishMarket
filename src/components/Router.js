524fbfde0fe95157494c4d0c161c2b112072abbb
Micheal Waldrup <waldrupm@gmail.com>
2019-10-16 14:47:03 -0400

    Refactor to function component, use props

---
+++ src/components/Router.js
@@ -0,0 +1,9 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Router = () => {
+  <BrowserRouter>
+    <Switch>
+      <Route> </Route>
+    </Switch>
+  </BrowserRouter>
+}
