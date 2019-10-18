6a85587a005ae7443560757e4bb90e5c9094642d
Micheal Waldrup <waldrupm@gmail.com>
2019-10-18 08:40:46 -0400

    Add ref to grab store name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.myInput = React.createRef();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(event) {
+    event.preventDefault();
+    const storeName = this.myInput.current.defaultValue;
+    this.props.history.push(`/store/${storeName}`);
+  }
 
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={this.myInput} />
         <button type="submit">Visit Store</button>
       </form>
     )
