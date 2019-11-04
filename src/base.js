f125376a62e6d08b891c5bd000d6dffeb2170716
Micheal Waldrup <waldrupm@gmail.com>
2019-11-04 10:31:10 -0500

    Remove Dist, add to Gitignore

--- src/base.js
+++ src/base.js
@@ -1,6 +1,7 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
+// API key will be on Github, on free plan so access would just be locked down if someone abused it
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyAHy8ukbpv75cTw4VAQh237HbLScCAGqp0",
   authDomain: "catch-of-the-day-1337.firebaseapp.com",
